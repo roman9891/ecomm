1ac01c976daff433484f35964e9768c09843c8cb
Roman Opalacz <roman9891@gmail.com>
2022-03-14 13:25:51 -0400

    updated post route

--- index.js
+++ index.js
@@ -17,9 +17,14 @@ app.get('/', (req, res) => {
     `)
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.send("hi there")
+app.post('/', async (req, res) => {
+    const { email, password, passwordConfirmation } = req.body
+    const existingUser = await userRepo.getOneBy({ email })
+
+    if (existingUser) return res.send('Sorry, Bro. That email is already in use')
+    if (password !== passwordConfirmation) return res.send("Sorry, Bro. Those passwords didn't match")
+
+    res.send("No way, Bro! You passed all the checks. You're account is the real deal now. You're the greatest that ever did it.")
 })
 
 app.put('/', (req, res) => {
